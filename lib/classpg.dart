b95418873f06599934b8695750f622efb1d4989e
Vijay Gawhale <vijay@vijaygahalesAir.Dlink>
2023-05-09 16:43:22 +0530

    Commiting files

---
+++ lib/classpg.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class classpg extends StatelessWidget {
+  const classpg({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Sumita"),
+    );
+  }
+}
